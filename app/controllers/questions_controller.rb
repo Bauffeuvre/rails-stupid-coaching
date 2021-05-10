22648b81a1eaa5917464091a26719841bec416c9
baptiste auffeuvre <baptiste.auffeuvre@gmail.com>
2021-05-10 12:02:29 -0400

    CF

--- app/controllers/questions_controller.rb
+++ app/controllers/questions_controller.rb
@@ -1,7 +1,18 @@
 class QuestionsController < ApplicationController
 
   def ask
-  
+    # raise
+    # redirect_to answer_path
   end
-
+  
+  def answer
+    @question = params[:question]
+    if @question == "I am going yo work"
+      @answer = "Great!"
+    elsif @question.include? "?"
+      @answer = "Silly question, get dressed and go to work!"
+    else
+      @answer = "I don't care, get dressed and go to work!"
+    end
+  end
 end
